97eca3d6e3ad6b5bf5db69c9ceb66caaed7df19a
euanseow <euanseow@gmail.com>
2023-11-04 02:04:35 -0400

    small edits

--- cdeez/cdeez/utils.py
+++ cdeez/cdeez/utils.py
@@ -9,15 +9,34 @@ class Driver:
 		self.client = MongoClient(server_settings['connection_string'])
 		self.mongoDB = self.client[server_settings['db_name']]
 		self.coursesDB = self.mongoDB.Courses
+		self.majorsDB = self.mongoDB.Majors
 
-	def addCourse(self, id, title, term, prerequisites):
+	def addCourse(self, id, name, terms, prereqs, subject):
 		post = {
 			"_id":id,
-			"title":title,
-			"term":term
+			"name": name,
+			"terms": terms,
+			"prereqs": prereqs,
+			"subject": subject
 		}
-		post_id = self.coursesDB.insert_one(post).inserted_id
+		return self.coursesDB.insert_one(post).inserted_id
 	
-	def getCourse(self, id):
+	def getCourseByID(self, id):
 		course = self.coursesDB.find_one({"_id":id})
 		return course
+	
+	def addMajor(self, id, name, discipline, premajor, core, electives, credits):
+		post = {
+			"_id": id,
+			"name": name,
+			"discipline": discipline,
+			"premajor": premajor,
+			"core": core,
+			"electives": electives,
+			"credits": credits
+		}
+		return self.majorsDB.insert_one(post).inserted_id
+	
+	def getMajorByID(self, id):
+		major = self.majorsDB.find_one({"_id":id})
+		return major
