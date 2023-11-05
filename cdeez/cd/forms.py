f7f126e7f6342a09d562566c2d85889209f14082
lucy <lucymy1225@gmail.com>
2023-11-05 08:43:58 -0500

    committtt

--- cdeez/cd/forms.py
+++ cdeez/cd/forms.py
@@ -22,6 +22,7 @@ class BooleanField(forms.BooleanField):
 driver = Driver()
 
 class LoginForm(forms.Form):
+
 	username = forms.CharField(label = 'Username', required = True)
 	password = forms.CharField(label = 'Password', widget = forms.PasswordInput(), required = True)
 
